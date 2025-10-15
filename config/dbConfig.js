00beec3916fff603853d694497bb61888afb5f14
Oskar Wong <oskar.wong@me.com>
2025-10-15 15:11:18 +0800

    local dev

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,8 @@
 
 module.exports = {
   mysql: {
-    host: process.env.MYSQL_HOST || 'localhost',
+    host: process.env.MYSQL_HOST || 'ai02-mac-mini.local',
+    port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'appuser',
     password: process.env.MYSQL_PASSWORD || '1A2B3c4d5e6f',
     database: process.env.MYSQL_DATABASE || 'appdb',
@@ -12,7 +13,7 @@ module.exports = {
     queueLimit: 0
   },
   mongodb: {
-    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/teleprep_db',
+    uri: process.env.MONGODB_URI || 'mongodb://ai02-mac-mini.local:27017/teleprep_db',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true
